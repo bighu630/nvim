f7dcd2562b3d3921f4811f88382714c33ae3251b
ivhu <ivhu@foxmail.com>
2023-01-03 15:36:49 +0800

    nil

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -193,7 +193,7 @@ end
 function config.lang_go()
 	vim.g.go_doc_keywordprg_enabled = 0
 	vim.g.go_def_mapping_enabled = 0
-	-- vim.g.go_code_completion_enabled = 0
+	vim.g.go_code_completion_enabled = 0
 end
 
 -- function config.lang_org()
