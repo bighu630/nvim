24fd24dc2697a82874293275f9f3fe6545a31628
ivhu <ivhu@foxmail.com>
2023-01-06 15:12:23 +0800

    nil

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -812,7 +812,7 @@ function config.symbols_outline()
 		show_symbol_details = true,
 		preview_bg_highlight = "Pmenu",
 		autofold_depth = nil,
-		auto_unfold_hover = true,
+		auto_unfold_hover = false,
 		fold_markers = { "", "" },
 		wrap = false,
 		keymaps = { -- These keymaps can be a string or a table for multiple keys
@@ -931,12 +931,13 @@ function config.todo()
 end
 
 function config.session()
+	local Path = require("plenary.path")
 	require("session_manager").setup({
-		-- sessions_dir = Path:new(vim.fn.stdpath("data"), "sessions"), -- The directory where the session files will be saved.
+		sessions_dir = Path:new(vim.fn.stdpath("data"), "sessions"), -- The directory where the session files will be saved.
 		path_replacer = "__", -- The character to which the path separator will be replaced for session files.
 		colon_replacer = "++", -- The character to which the colon symbol will be replaced for session files.
 		autoload_mode = require("session_manager.config").AutoloadMode.Disabled, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
-		autosave_last_session = false, -- Automatically save last session on exit and on session switch.
+		autosave_last_session = true, -- Automatically save last session on exit and on session switch.
 		autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
 		autosave_ignore_filetypes = { -- All buffers of these file types will be closed before the session is saved.
 			"gitcommit",
@@ -946,6 +947,12 @@ function config.session()
 		autosave_only_in_session = true, -- Always autosaves session. If true, only autosaves after a session is active.
 		max_path_length = 80, -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
 	})
+	vim.cmd([[
+    augroup _open_nvim_tree
+        autocmd! * <buffer>
+        autocmd SessionLoadPost * silent! lua require("nvim-tree").toggle(false, true)
+    augroup end
+    ]])
 end
 
 return config
