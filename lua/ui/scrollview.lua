3c990b2a0a3489a9cd1a941d094474f4fa112d6b
胡腾 <ivhu@foxmail.com>
2023-09-11 16:39:36 +0800

    chore: 开启imp标记符号

--- lua/ui/scrollview.lua
+++ lua/ui/scrollview.lua
@@ -1,19 +1,7 @@
 local config = {}
 
 function config.scrollview()
-    local icons = { diagnostics = require("ui.icons").get("diagnostics", true) }
-
-    require("scrollview").setup({
-        scrollview_mode = "virtual",
-        excluded_filetypes = { "NvimTree", "terminal", "nofile", "Outline" },
-        winblend = 0,
-        signs_on_startup = { "diagnostics", "folds", "marks", "search", "spell" },
-        diagnostics_error_symbol = icons.diagnostics.Error,
-        diagnostics_warn_symbol = icons.diagnostics.Warning,
-        diagnostics_info_symbol = icons.diagnostics.Information,
-        diagnostics_hint_symbol = icons.diagnostics.Hint,
-
-    })
+    require("scrollbar").setup()
 end
 
 return config
