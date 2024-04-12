f9f9665898e261b5aeaec4e1efc4147cb04d6e98
ivhu <ivhu@foxmail.com>
2024-04-12 14:26:06 +0800

    fech:对接账户服务签名模块

--- lua/dap/dap-adapter.lua
+++ lua/dap/dap-adapter.lua
@@ -2,7 +2,7 @@ local dap = require("dap")
 
 dap.adapters.go = {
 	type = "server",
-	port = math.random(20000, 30000),
+	port = "${port}",
 	executable = {
 		command = "dlv",
 		args = { "dap", "-l", "127.0.0.1:${port}" },
@@ -30,10 +30,10 @@ dap.adapters.bashdb = {
 dap.adapters.pwa_node = {
 	type = "server",
 	host = "localhost",
-	port = math.random(20000, 30000),
+	port = "${port}",
 	executable = {
 		command = "node",
 		-- 💀 Make sure to update this path to point to your installation
-		args = { vim.env.HOME .. "/.vscode/extensions/js-debug/src/dapDebugServer.js", port },
+		args = { vim.env.HOME .. "/.vscode/extensions/js-debug/src/dapDebugServer.js", "${port}" },
 	},
 }
