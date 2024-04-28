68d8801a94c48585dd223f141f0d4cf310d962e9
ivhu <ivhu@foxmail.com>
2024-04-28 15:31:17 +0800

    chore:设置go tag 为驼峰命名

--- lua/lang/go.lua
+++ lua/lang/go.lua
@@ -16,6 +16,7 @@ function config.vim_go()
 	vim.g.go_mod_fmt_autosave = 1
 	vim.g.go_doc_keywordprg_enabled = 0
 	vim.g.go_def_mapping_enabled = 0
+	vim.g.go_addtags_transform = "camelcase"
 end
 
 return config
