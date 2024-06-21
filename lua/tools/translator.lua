b1c1acc53b694d72f799caed7a1df0024ea41b44
ivhu <ivhu@foxmail.com>
2024-06-21 08:43:58 +0800

    chore：更新gitsigns

--- lua/tools/translator.lua
+++ lua/tools/translator.lua
@@ -1,9 +1,10 @@
 local config = {}
 
 function config.translator()
-    vim.cmd([[
+	vim.cmd([[
     let g:translator_default_engines = ['google']
     let g:translator_window_borderchars = ["─","│","─","│","╭","╮","╯","╰"]
+    " let g:translator_proxy_url = 'http://192.168.3.244:7890'
     ]])
 end
 
--- lua/ui/gitsigns.lua
+++ lua/ui/gitsigns.lua
@@ -1,37 +1,45 @@
 local config = {}
 function config.gitsigns()
 	require("gitsigns").setup({
+		-- signs = {
+		-- 	add = {
+		-- 		hl = "GitSignsAdd",
+		-- 		text = "│",
+		-- 		numhl = "GitSignsAddNr",
+		-- 		-- linehl = "GitSignsAddLn",
+		-- 	},
+		-- 	change = {
+		-- 		hl = "GitSignsChange",
+		-- 		text = "│",
+		-- 		numhl = "GitSignsChangeNr",
+		-- 		linehl = "GitSignsChangeLn",
+		-- 	},
+		-- 	delete = {
+		-- 		hl = "GitSignsDelete",
+		-- 		text = "_",
+		-- 		numhl = "GitSignsDeleteNr",
+		-- 		linehl = "GitSignsDeleteLn",
+		-- 	},
+		-- 	topdelete = {
+		-- 		hl = "GitSignsDelete",
+		-- 		text = "‾",
+		-- 		numhl = "GitSignsDeleteNr",
+		-- 		linehl = "GitSignsDeleteLn",
+		-- 	},
+		-- 	changedelete = {
+		-- 		hl = "GitSignsChange",
+		-- 		text = "~",
+		-- 		numhl = "GitSignsChangeNr",
+		-- 		linehl = "GitSignsChangeLn",
+		-- 	},
+		-- },
 		signs = {
-			add = {
-				hl = "GitSignsAdd",
-				text = "│",
-				numhl = "GitSignsAddNr",
-				linehl = "GitSignsAddLn",
-			},
-			change = {
-				hl = "GitSignsChange",
-				text = "│",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
-			delete = {
-				hl = "GitSignsDelete",
-				text = "_",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			topdelete = {
-				hl = "GitSignsDelete",
-				text = "‾",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			changedelete = {
-				hl = "GitSignsChange",
-				text = "~",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
 		},
 		watch_gitdir = { interval = 1000, follow_files = true },
 		current_line_blame = true,
