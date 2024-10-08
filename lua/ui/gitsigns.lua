8ac9177a1231fe4ae2f89f572bbf54f2e9b917a7
ivhu <ivhu@foxmail.com>
2024-10-08 11:33:08 +0800

    chore:更早的显示当前行修改

--- lua/ui/gitsigns.lua
+++ lua/ui/gitsigns.lua
@@ -43,7 +43,7 @@ function config.gitsigns()
 		},
 		watch_gitdir = { interval = 1000, follow_files = true },
 		current_line_blame = true,
-		current_line_blame_opts = { delay = 3000, virtual_text_pos = "eol" },
+		current_line_blame_opts = { delay = 1000, virtual_text_pos = "eol" },
 		sign_priority = 6,
 		update_debounce = 100,
 		status_formatter = nil, -- Use default
