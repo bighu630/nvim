e73267a7f535afc7119becf6a8f8258937569d0d
bighu630 <ivhu@foxmail.com>
2026-09-08 08:03:48 +0800

    chore: Automated update

--- lua/tools/init.lua
+++ lua/tools/init.lua
@@ -9,6 +9,18 @@ return {
 		cmd = "Leet",
 		opts = require("tools.leetcode"),
 	},
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1001, -- this plugin needs to run before anything else
+		opts = {
+			rocks = { "magick" },
+		},
+	},
+	{
+		"3rd/image.nvim",
+		dependencies = { "luarocks.nvim" },
+		opts = {},
+	},
 	-- {
 	-- 	"chipsenkbeil/distant.nvim",
 	-- 	branch = "v0.3",
